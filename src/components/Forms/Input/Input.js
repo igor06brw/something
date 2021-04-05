e972504873f4c29b0911209d85cdda8ccb7a8f55
Igor <igor06brw@gmail.com>
2021-04-05 18:45:04 +0200

    finish inputs

--- src/components/Forms/Input/Input.js
+++ src/components/Forms/Input/Input.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 
 const InputContainer = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    padding: 10px;
 `
 const Label = styled.label`
-
+    padding-bottom: 10px;
 `
 const InputField = styled.input`
 
 `
 
-const Input = ({ value, label, name, type, placeholder }) => {
+const Input = ({ value, label, forName, type, placeholder }) => {
+    const handleChange = (event) => {
+        value(event.target.value)
+    }
+
     return (
         <InputContainer>
-            <Label>{label}</Label>
-            <InputField />
+            <Label for={forName}>{label}</Label>
+            <InputField name={forName} type={type} placeholder={placeholder} onChange={(e) => handleChange(e)} />
         </InputContainer>
     );
 }
--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Input from '../components/Forms/Input/Input';
 
 
-const SignUp = () => {
+const SignUp = ({ value  }) => {
+
+    const handleChange = (event) => {
+        console.log(event)
+    }
     return (
         <div>
-            <Input label={'E-mail'}/>
+            <Input label={'E-mail'} forName={'signUpEmail'} type={"email"} placeholder="press your e-mail..." value={handleChange} />
+            <Input label={'Password'} forName={'signUpPassword'} type={"password"} placeholder="press your password..." value={handleChange} />
+        
         </div>
     );
 }

f5eadd75a60205c34553b05760739807cbfe3e3f
Igor <igor06brw@gmail.com>
2021-04-05 11:22:15 +0200

    init redux firebase project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBqiQ5PZ9RLU4g99JI3vZjj6X_WK9-6j5Q",
+  authDomain: "test-project-31aa6.firebaseapp.com",
+  projectId: "test-project-31aa6",
+  storageBucket: "test-project-31aa6.appspot.com",
+  messagingSenderId: "841132426263",
+  appId: "1:841132426263:web:7cd70616a40466f01b8852"
+};

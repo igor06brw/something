666e52b6642e6d35af847659b215d2dfab0f9b07
Igor <igor06brw@gmail.com>
2021-04-06 17:02:39 +0200

    create hoc form, init in SignUp

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Input from '../components/Forms/Input/Input';
+import Button from '../components/Forms/Button/Button';
 
+import { StyledForm } from '../hoc/form';
 
-const SignUp = ({ value  }) => {
+const SignUp = ({ value }) => {
 
     const handleChange = (event) => {
         console.log(event)
     }
     return (
-        <div>
+        <StyledForm>
             <Input label={'E-mail'} forName={'signUpEmail'} type={"email"} placeholder="press your e-mail..." value={handleChange} />
             <Input label={'Password'} forName={'signUpPassword'} type={"password"} placeholder="press your password..." value={handleChange} />
-        
-        </div>
+            <Button>Sign Up</Button>
+        </StyledForm>
     );
 }
 
---
+++ src/hoc/form.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledForm = styled.form`
+    display: flex;
+    align-items: center;
+    flex-direction: column;    
+`

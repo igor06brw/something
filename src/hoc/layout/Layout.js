e0bc456988818e6ad344dc54cbbdb5dfadb38f75
Igor <igor06brw@gmail.com>
2021-04-06 17:02:54 +0200

    start writing layout  comp

---
+++ src/hoc/layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LayoutWrapper = styled.
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Layout;

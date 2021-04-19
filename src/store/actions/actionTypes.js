e1a05cffe77c608ef0be2176b25180e77e05d446
Igor <f56JFS20ff@protonmail.com>
2021-04-19 06:14:56 +0200

    create actionTypes

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,4 @@
+export const SIGN_UP_START = "SIGN_UP_START";
+export const SIGN_UP_PENDING = "SIGN_UP_PENDING";
+export const SIGN_UP_SUCCESSFULL = "SIGN_UP_SUCCESSFULL";
+export const SIGN_UP_ERROR = "SIGN_UP_ERROR";

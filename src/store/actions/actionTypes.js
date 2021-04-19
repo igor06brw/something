c6d51099f3d99f8a18fda6594857514ca24253d8
Igor <f56JFS20ff@protonmail.com>
2021-04-19 06:17:45 +0200

    edit actionTypes & add it to authActions

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,4 +1,3 @@
-export const SIGN_UP_START = "SIGN_UP_START";
-export const SIGN_UP_PENDING = "SIGN_UP_PENDING";
+export const SIGN_UP_START = "SIGN_UP_START"; 
 export const SIGN_UP_SUCCESSFULL = "SIGN_UP_SUCCESSFULL";
 export const SIGN_UP_ERROR = "SIGN_UP_ERROR";
--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,17 +1,19 @@
+import * as actions from './actionTypes';
+
 export const signUp = user => {
   return (dispatch, getState, { getFirebase }) => {
     const firebase = getFirebase();
     console.log(user);
-    dispatch({ type: "SIGN_UP_START" });
+    dispatch({ type: actions.SIGN_UP_START });
     
     firebase
-      .auth()
-      .createUserWithEmailAndPassword(user.signUpEmail, user.signUpPassword)
-      .then(() => {
-        dispatch({ type: "SIGN_UP" });
+    .auth()
+    .createUserWithEmailAndPassword(user.signUpEmail, user.signUpPassword)
+    .then(() => {
+      dispatch({ type: actions.SIGN_UP_SUCCESSFULL });
       })
       .catch(err => {
-        dispatch({ type: "SIGN_UP_ERR" }, err);
+        dispatch({ type: actions.SIGN_UP_ERROR }, err);
       });
   };
 };

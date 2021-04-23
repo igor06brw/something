339898269c750a40ff7fdd921b06437b878d51a2
Igor <f56JFS20ff@protonmail.com>
2021-04-23 16:53:53 +0200

    add route

--- src/App.js
+++ src/App.js
@@ -4,13 +4,37 @@ import { Switch, Route, Link } from "react-router-dom";
 import Layout from './components/Layout/Layout';
 import SignUp from './containers/SignUp';
 import SignIn from './containers/SignIn';
+import SignOut from './containers/SignOut';
 
-function App() {
+import { useSelector } from 'react-redux';
+
+const App = () => {
+  let routes;
+  
+  const auth = useSelector(state => state.firebase.auth);
+  
+  
+  if(auth.uid !== undefined) {
+    routes = (
+      <Switch>
+        <SignOut />
+      </Switch>
+    ) 
+  } else {
+      routes = (
+        <Switch>
+          <div>
+            <SignIn />
+            <SignUp />
+          </div>
+        </Switch>
+      )
+  }
+
   return (
       <div className="App">
         <Layout>
-          <SignIn />
-          <SignUp />
+          {routes}
         </Layout>
       </div>
   );

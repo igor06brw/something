9516743d32e77e58fa4e507c7add3ec0a8305f91
Igor <f56JFS20ff@protonmail.com>
2021-04-23 16:19:44 +0200

    add new action types

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -5,3 +5,7 @@ export const SIGN_UP_ERROR = "SIGN_UP_ERROR";
 export const SIGN_IN_START = "SIGN_IN_START"; 
 export const SIGN_IN_SUCCESSFULL = "SIGN_IN_SUCCESSFULL";
 export const SIGN_IN_ERROR = "SIGN_IN_ERROR";
+
+export const SIGN_IN_START = "SIGN_OUT_START"; 
+export const SIGN_IN_SUCCESSFULL = "SIGN_OUT_SUCCESSFULL";
+export const SIGN_IN_ERROR = "SIGN_OUT_ERROR";
